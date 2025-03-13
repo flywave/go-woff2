db986b27bdcc8e7c7776afb8aa9d0024ba34bd85
aninggo <aninggo@gmail.com>
2025-03-13 18:35:27 +0800

    u

--- woff2.go
+++ woff2.go
@@ -7,6 +7,6 @@ package woff2
 // #cgo CXXFLAGS: -I ./lib
 // #cgo linux LDFLAGS:  -L ./lib/linux -Wl,--start-group  -lstdc++ -lm -pthread -lbrotlicommon -lbrotlidec -lbrotlienc -lwoff2common -lwoff2dec -lwoff2enc -lc_woff2 -Wl,--end-group
 // #cgo windows LDFLAGS: -L ./lib/windows  -Wl,--start-group  -lstdc++ -lbrotlicommon -lbrotlidec -lbrotlienc -lwoff2common -lwoff2dec -lwoff2enc -lc_woff2 -Wl,--end-group
-// #cgo darwin LDFLAGS: -L　./lib/darwin -lbrotlicommon -lbrotlidec -lbrotlienc -lwoff2common -lwoff2dec -lwoff2enc -lc_woff2
-// #cgo darwin,arm LDFLAGS: -L　./lib/darwin_arm -lbrotlicommon -lbrotlidec -lbrotlienc -lwoff2common -lwoff2dec -lwoff2enc -lc_woff2
+// #cgo darwin,amd64 LDFLAGS: -L　./lib/darwin -lbrotlicommon -lbrotlidec -lbrotlienc -lwoff2common -lwoff2dec -lwoff2enc -lc_woff2 -lc++
+// #cgo darwin,arm64 LDFLAGS: -L　./lib/darwin_arm -lbrotlicommon -lbrotlidec -lbrotlienc -lwoff2common -lwoff2dec -lwoff2enc -lc_woff2 -lc++
 import "C"
